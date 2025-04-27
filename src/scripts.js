afa1e44a0bffb6a275b5377951de3f032532deef
NataliaMuthanga <muthanga.natalia@gmail.com>
2025-04-27 10:38:28 +0300

    Initialize variables for managing contact information in the address book

---
+++ src/scripts.js
@@ -0,0 +1,5 @@
+let people = JSON.parse(localStorage.getItem("people")) || [];
+let fullName = document.getElementById("fullname");
+let phoneNumber = document.getElementById("number");
+let email = document.getElementById("email");
+

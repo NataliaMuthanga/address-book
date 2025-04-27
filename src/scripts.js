5397258040c836516a9d303ac8a30621056fa7d0
NataliaMuthanga <muthanga.natalia@gmail.com>
2025-04-27 10:48:48 +0300

    Variable declarations for address form and contacts

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
 let people = JSON.parse(localStorage.getItem("people")) || [];
-let fullName = document.getElementById("fullname");
-let phoneNumber = document.getElementById("number");
-let email = document.getElementById("email");
-
+let addressFm = document.getElementById("address-fm");
+let yourContacts = document.getElementById("yourContacts");

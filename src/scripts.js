67098876bd1e50aaf6f9ee4c4db603ae99cd03eb
NataliaMuthanga <muthanga.natalia@gmail.com>
2025-04-27 10:56:34 +0300

    Refactor form submission handling in address book

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,11 @@
 let people = JSON.parse(localStorage.getItem("people")) || [];
 let addressFm = document.getElementById("address-fm");
 let yourContacts = document.getElementById("yourContacts");
+
+addressFm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const fullName = document.getElementById("fullname").value.trim();
+    const phoneNumber = document.getElementById("munber").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const profession = document.getElementById("profession").value.trim();
+})

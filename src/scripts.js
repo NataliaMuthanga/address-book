76ff21d4854eaf7ab649b05cb1389b136f4a63a1
NataliaMuthanga <muthanga.natalia@gmail.com>
2025-04-27 11:09:28 +0300

    Fix typo in phone number input ID and refactor person object creation in form submission

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,18 @@ let yourContacts = document.getElementById("yourContacts");
 addressFm.addEventListener("submit", function (event) {
     event.preventDefault();
     const fullName = document.getElementById("fullname").value.trim();
-    const phoneNumber = document.getElementById("munber").value.trim();
+    const phoneNumber = document.getElementById("nunber").value.trim();
     const email = document.getElementById("email").value.trim();
     const profession = document.getElementById("profession").value.trim();
+
+    const person = {
+        fullName: fullName,
+        phoneNumber: phoneNumber,
+        email: email,
+        profession: profession
+    };
+    people.push(person);
+    localStorage.setItem("people", JSON.stringify(people));
+    addressFm.reset();
+    displayPeople(people);
 })
